ef46f0e5a002ac727a74c4e86464032ab49058d2
David Ng <david.ng.dev@gmail.com>
2017-05-17 00:24:37 +0800

    Implement download operexchangerate
    Integrate chai-as-promised that make test promise function painless

--- scripts/cron/importOpenExchangeRate.js
+++ scripts/cron/importOpenExchangeRate.js
@@ -1,13 +1,30 @@
 /**
  * Created by DavidNg on 15/5/2017.
  */
-function importOpenExchangeRate(startDate,endDate){
-  console.log(startDate.format());
-  console.log(endDate.format());
+const moment = require('moment');
+
+const rp = require('request-promise');
+
+function importOpenExchangeRate(importDate){
+  importDate = moment(importDate).format('YYYY-MM-DD');
+  console.log(importDate);
+  return new Promise((resolve, reject)=>{
+    rp.get({
+      url: `https://openexchangerates.org/api/historical/${importDate}.json`,
+      qs: {
+        app_id: process.env.OPEN_EXCHANGE_RATE_APP_ID
+      },
+      json: true
+    }).then((repos)=>{
+      resolve(repos);
+      //TODO: Import result to DB
+    }).catch(reject);
+    }
+  );
 }
 
 if (require.main === module) {
-  importOpenExchangeRate();
+  importOpenExchangeRate(new Date());
 }
 
 module.exports = importOpenExchangeRate;
--- tests/myTest.js
+++ tests/myTest.js
@@ -5,14 +5,16 @@ process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const chaiAsPromised = require("chai-as-promised");
+
 const app = require('../app');
 const should = chai.should();
-
 const moment = require('moment');
 
 const importOpenExchangeRate = require('../scripts/cron/importOpenExchangeRate');
 
 chai.use(chaiHttp);
+chai.use(chaiAsPromised);
 
 //TODO: Replace me!!!!!!!!!!!!!!!!!!!!!
 describe('Init Testing', () => {
@@ -36,9 +38,12 @@ describe('Init Testing', () => {
 
 describe('Test Open Exchange Rate API', ()=>{
   it('Should import open exchange rate to DB', ()=>{
-    importOpenExchangeRate(
-      moment().subtract(10, 'days'),
-      moment()
-    );
+    return importOpenExchangeRate(
+      yesterday()
+    ).should.be.fulfilled;
   });
 });
+
+function yesterday(){
+  return moment().subtract(1,'days');
+}
