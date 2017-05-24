8aa1c35948fa94e9bd97a008eef601fce31481e9
David Ng <david.ng.dev@gmail.com>
2017-05-25 03:13:24 +0800

    Add IAM to AWS auto create script
    Auto create IAM role by script.
    Default permission of role will be admin access.
    If you don't like it please manually update after execute script

--- README.md
+++ README.md
@@ -8,6 +8,15 @@
 
 
 ## Installation
+
+Before installation please ensure your local machine has been install and configured aws cli
+
+[AWS cli installation](http://docs.aws.amazon.com/cli/latest/userguide/installing.html)
+
+[AWS cli configuration](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html)
+
+Noted that your aws account should have AdminAccess if you want automated init aws environment by script. 
+
 ```
 1. npm install
 2. copy config/.env.example into config/.env and fill in the blank
@@ -30,3 +39,50 @@
     6. Never use boolean flag parameter
     7. No useless comments
     8. After deployment you must manually update script/enviroment/aws-init.js LAMBDA_SOURCE_CODE_PATH variable
+    9. Indention level and length of chain should less as much as possible, maximum level must less than 3 
+        Correct:
+            if (foo){
+              //Indention level 1
+            }
+            if (foo){
+              if (bar){
+                //Indention level 2
+              }
+            }
+            Promise(foo)
+              .then(bar) //Chain size is 1
+            Promise(foo)
+              .then(bar)
+              .then(foobar) //Chain size is 2
+        Incorrect:
+            if (foo){
+              if (bar){
+                if (foobar){
+                  //Indention level 3
+                }
+              }
+            }
+            Promise(foo)
+              .then(bar)
+              .then(foobar)
+              .then(foo) //Chain size is 3
+    10. Object key no quote unless necessary 
+        Correct:
+          {
+            a : 1,
+            b: 2
+          }
+        Incorrect:
+          {
+            'a': 1,
+            'b': 2
+          }
+     11. Module import should follow that order: build-in, third-party, custom and between each type of module should have 1 empty line break
+        Correct:
+          require('path')
+          
+          require('express')
+          
+          require('./app')
+          
+            
--- script/enviroment/aws-init.js
+++ script/enviroment/aws-init.js
@@ -1,46 +1,113 @@
 #!/usr/bin/env node
 'use strict'
+const querystring = require('querystring');
+
 const AWS = require('aws-sdk');
 const config = require('../../config');
 
+//Variable prefix is AWS service name
+const DYNAMO_DB_TABLE = 'ExchangeRates';
+const S3_LAMBDA_SOURCE_CODE_BUCKET = 'dng-dev-resources';
+const S3_LAMBDA_SOURCE_CODE_PATH = 'lambda-codes/open-exchange-backend/openexchange-backend-2017052404-v0.0.2.zip';
+const LAMBDA_SERVERLESS_EXPRESS_FUNCTION_NAME = 'currencyExchangeBackend';
+const LAMBDA_DOWNLOAD_EXCHANGE_RATE_CRON_JOB_NAME = 'downloadOpenExchangeRate' ;
+const API_GATEWAY_API_NAME = 'Currency exchange backend';
+const CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME = 'night_jobs';
+const IAM_ROLE_NAME = 'intranet';
+const IAM_ROLE_ARN = `arn:aws:iam::${config.env.AWS_ACCOUNT}:role/${IAM_ROLE_NAME}`;
+/**
+ * WARNING!
+ * Role with this access will available to do anything under AWS environment such add/delete service, update service settings ...etc.
+ * If you don't take a risk you can modify them after created by script.
+ * */
+const IAM_ROLE_POLICY = 'arn:aws:iam::aws:policy/AdministratorAccess';
+const IAM_ROLE_TRUST_RELATIONSHIP = JSON.stringify({
+  Version: '2012-10-17',
+  Statement: [
+    {
+      Effect: 'Allow',
+      Principal: {
+        Service: 'ec2.amazonaws.com'
+      },
+      Action: 'sts:AssumeRole'
+    },
+    {
+      Effect: 'Allow',
+      Principal: {
+        Service: 'lambda.amazonaws.com'
+      },
+      Action: 'sts:AssumeRole'
+    }
+  ]
+});
+
+Promise.resolve = Promise.resolve.bind(Promise);
+Promise.reject = Promise.reject.bind(Promise);
+const iam = new AWS.IAM();
 const dynamodb = new AWS.DynamoDB();
 const lambda = new AWS.Lambda();
 const apigateway = new AWS.APIGateway();
 const cloudwatchevents = new AWS.CloudWatchEvents();
 
-const DB_TABLE = 'ExchangeRates';
-const EXPRESS_LAMBDA_FUNCTION = 'currencyExchangeBackend';
-const API_NAME = 'Currency exchange backend';
-const LAMBDA_SOURCE_CODE_PATH = 'lambda-codes/open-exchange-backend/openexchange-backend-2017052404-v0.0.2.zip';
-const CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME = 'night_jobs';
 
-Promise.resolve = Promise.resolve.bind(Promise);
-Promise.reject = Promise.reject.bind(Promise);
 
 if (require.main === module) {
-  createAWSEnvironment().then(()=>{
+  constructAWSEnvironment().then(()=>{
     console.log('Success create AWS');
   }).catch((err)=>{
     if (err){
+      console.log('Unexpected Error!');
       console.log(err);
-      return;
+      process.exit(-1);
     }
-    console.log('Environment already created');
+    console.log('Environment already constructed');
   });
 }
 
-function createAWSEnvironment(){
-  let initJobs = [];
-  // initJobs.push(initDynamoDB());
-  // initJobs.push(initExpressLambda());
-  // initJobs.push(initAPIGateWay());
-  initJobs.push(createSchedulerOnCloudWatchEvent());
-  initJobs.push(initCronJobLambda());
-  return seriesPromise(initJobs);
+function constructAWSEnvironment(){
+  let constructAWSJobs = [];
+  constructAWSJobs.push(constructIAMRole());
+  constructAWSJobs.push(constructSchedulerOnCloudWatchEvent());
+  constructAWSJobs.push(constructCronJobLambda());
+  constructAWSJobs.push(constructDynamoDB());
+  constructAWSJobs.push(constructExpressLambda());
+  constructAWSJobs.push(constructAPIGateWay());
+  return seriesPromise(constructAWSJobs);
 }
 
-function initDynamoDB(){
-  shouldCreateDynamoDBTable().then(()=>{
+function constructIAMRole(){
+  return shouldCreateIAMRole().then(()=>{
+    return iam.createRole({
+      AssumeRolePolicyDocument: IAM_ROLE_TRUST_RELATIONSHIP,
+      RoleName: IAM_ROLE_NAME
+    }).promise()
+      .then(()=>{
+        return iam.attachRolePolicy({
+          PolicyArn: IAM_ROLE_POLICY,
+          RoleName: IAM_ROLE_NAME
+        }).promise();
+      });
+  });
+}
+
+function shouldCreateIAMRole(){
+  return new Promise((resolve,reject)=>{
+    iam.listRoles({
+      PathPrefix: '/'
+    }).promise().then((data)=>{
+      for (let role of data.Roles){
+        if (role.Arn === IAM_ROLE_ARN){
+          reject();
+          return;
+        }
+        resolve();
+      }
+    })
+  });
+}
+
+function constructDynamoDB(){
+  return shouldCreateDynamoDBTable().then(()=>{
     return dynamodb.createTable({
       AttributeDefinitions:[
         {
@@ -66,52 +133,49 @@ function initDynamoDB(){
         ReadCapacityUnits: 5,
         WriteCapacityUnits: 5
       },
-      TableName: DB_TABLE
+      TableName: DYNAMO_DB_TABLE
     }).promise();
-  }).catch(()=>{
-    return Promise.resolve();//No error, Table already init
   });
 }
 
 function shouldCreateDynamoDBTable(){
   return dynamodb.describeTable({
-    TableName: DB_TABLE
-  }).promise().then(Promise.reject).catch(Promise.resolve);
+    TableName: DYNAMO_DB_TABLE
+  }).promise().then(()=>Promise.reject());
 }
 
-function initExpressLambda(){
-  return shouldCreateLambda(EXPRESS_LAMBDA_FUNCTION).then(()=>{
+function constructExpressLambda(){
+  return shouldCreateLambda(LAMBDA_SERVERLESS_EXPRESS_FUNCTION_NAME).then(()=>{
     return lambda.createFunction({
       Code: {
-        S3Bucket: 'dng-dev-resources',
-        S3Key: LAMBDA_SOURCE_CODE_PATH
+        S3Bucket: S3_LAMBDA_SOURCE_CODE_BUCKET,
+        S3Key: S3_LAMBDA_SOURCE_CODE_PATH
       },
-      FunctionName: EXPRESS_LAMBDA_FUNCTION,
+      FunctionName: LAMBDA_SERVERLESS_EXPRESS_FUNCTION_NAME,
       Handler: 'lambda.handler',
-      Role: 'arn:aws:iam::139227058951:role/intranet',
+      Role: IAM_ROLE_ARN,
       Runtime: 'nodejs6.10',
       Description: 'Currency exchange/convert API written in Express',
       MemorySize: 256,
       Publish: true,
       Timeout: 60,
     }).promise();
-  }).catch(Promise.resolve);
+  });
 }
 
 function shouldCreateLambda(lambdaFunctionName){
   return lambda.getFunction({
     FunctionName: lambdaFunctionName
   }).promise()
-    .then(Promise.reject)
-    .catch(Promise.resolve);
+    .then(()=>Promise.reject());
 }
 
-function initAPIGateWay(){
-  //TODO: Refactor this function. It too long and painful to read
+function constructAPIGateWay(){
+  //TODO: Refactor this function. It too long and painful to understand
   //http://docs.aws.amazon.com/lambda/latest/dg/with-on-demand-https-example-configure-event-source.html
   return shouldSetupApiGateway().then(()=>{
     return apigateway.createRestApi({
-      name: API_NAME,
+      name: API_GATEWAY_API_NAME,
       description: 'API for list least/historical exchange rate'
     }).promise()
       .then((data)=>{
@@ -151,7 +215,7 @@ function initAPIGateWay(){
           type: 'AWS_PROXY',
           integrationHttpMethod: 'POST', //API Gateway uses to communicate with AWS Lambda
           passthroughBehavior: 'WHEN_NO_MATCH',
-          uri: `arn:aws:apigateway:${config.env.AWS_REGION}:lambda:path/2015-03-31/functions/arn:aws:lambda:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:function:${EXPRESS_LAMBDA_FUNCTION}/invocations`
+          uri: `arn:aws:apigateway:${config.env.AWS_REGION}:lambda:path/2015-03-31/functions/arn:aws:lambda:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:function:${LAMBDA_SERVERLESS_EXPRESS_FUNCTION_NAME}/invocations`
         }).promise().then(()=>{return Promise.resolve(data)});
       })
       .then((data)=>{
@@ -226,7 +290,7 @@ function initAPIGateWay(){
         //Enable APIGateway tester by allow tester invoke lambda
         return lambda.addPermission({
           Action: 'lambda:InvokeFunction',
-          FunctionName: EXPRESS_LAMBDA_FUNCTION,
+          FunctionName: LAMBDA_SERVERLESS_EXPRESS_FUNCTION_NAME,
           Principal: 'apigateway.amazonaws.com',
           SourceArn: `arn:aws:execute-api:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:${data.restApiId}/*/*/*`,
           StatementId: `apt-gateway-${data.restApiId}-lambda-integration-test`
@@ -244,7 +308,7 @@ function initAPIGateWay(){
           return Promise.resolve(data);
         });
       });
-  }).catch(Promise.resolve);
+  });
 }
 
 function shouldSetupApiGateway(){
@@ -253,7 +317,7 @@ function shouldSetupApiGateway(){
       limit: 500
     }).promise().then((data)=>{
       for (let item of data.items){
-        if (item.name === API_NAME){
+        if (item.name === API_GATEWAY_API_NAME){
           reject();
           return;
         }
@@ -276,43 +340,40 @@ function rootResourceSourceIDOfAPI(restApiId){
 }
 
 function seriesPromise(promiseJobs){
+  // TODO: Code will diplay UnhandledPromiseRejectionWarning while promiseJobs > 2, how to dismiss?
   return new Promise((resolve,reject)=>{
     let promiseWillSeriesExecute = Promise.resolve();
     promiseJobs.forEach(function(promiseJob){
       promiseWillSeriesExecute = promiseWillSeriesExecute
-        .then(()=>{return promiseJob;})
-        .catch((err)=>{
-          console.log(err);
-          reject(err);
-        });
+        .then(()=>{return promiseJob.catch(Promise.reject);})
+        .catch(Promise.reject);
     });
-    promiseWillSeriesExecute.then(resolve);
+    promiseWillSeriesExecute.then(resolve).catch(reject);
   })
 }
 
-function createSchedulerOnCloudWatchEvent(){
+function constructSchedulerOnCloudWatchEvent(){
   return shouldCreateCloudWatchScheduler()
     .then(()=>{
-      cloudwatchevents.putRule({
-        Name: CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME,
+      return cloudwatchevents.putRule({
+        Name: CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME,
         Description: 'Will trigger on HKT 4a.m',
         ScheduleExpression: 'cron(0 20 ? * * *)',
         State: 'ENABLED'
       }).promise()
-    })
-    .catch(Promise.resolve);
+    });
 }
 
 function shouldCreateCloudWatchScheduler(){
   return new Promise((resolve,reject)=>{
-    cloudwatchevents.listRules({
-      NamePrefix: CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME
+    return cloudwatchevents.listRules({
+      NamePrefix: CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME
     })
     .promise()
       .catch(resolve)
       .then((data)=>{
         for (let rule of data.Rules){
-          if (rule.Name === CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME){
+          if (rule.Name === CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME){
             reject();
             return;
           }
@@ -322,17 +383,16 @@ function shouldCreateCloudWatchScheduler(){
   });
 }
 
-function initCronJobLambda(){
-  const downloadExchangeRateCronJobName = 'downloadOpenExchangeRate' ;
-  return shouldCreateLambda(downloadExchangeRateCronJobName).then(()=>{
+function constructCronJobLambda(){
+  return shouldCreateLambda(LAMBDA_DOWNLOAD_EXCHANGE_RATE_CRON_JOB_NAME).then(()=>{
     return lambda.createFunction({
       Code: {
-        S3Bucket: 'dng-dev-resources',
-        S3Key: LAMBDA_SOURCE_CODE_PATH
+        S3Bucket: S3_LAMBDA_SOURCE_CODE_BUCKET,
+        S3Key: S3_LAMBDA_SOURCE_CODE_PATH
       },
-      FunctionName: downloadExchangeRateCronJobName,
+      FunctionName: LAMBDA_DOWNLOAD_EXCHANGE_RATE_CRON_JOB_NAME,
       Handler: 'lambdaCron.handler',
-      Role: 'arn:aws:iam::139227058951:role/intranet',
+      Role: IAM_ROLE_ARN,
       Runtime: 'nodejs6.10',
       Description: 'Will download exchange rate to dynamo DB',
       MemorySize: 512,
@@ -347,22 +407,22 @@ function initCronJobLambda(){
       .then(()=>{
         return lambda.addPermission({
           Action: 'lambda:InvokeFunction',
-          FunctionName: downloadExchangeRateCronJobName,
+          FunctionName: LAMBDA_DOWNLOAD_EXCHANGE_RATE_CRON_JOB_NAME,
           Principal: 'events.amazonaws.com',
-          SourceArn: `arn:aws:events:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:rule/${CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME}`,
-          StatementId: `cloud-watch-event-${CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME}`
+          SourceArn: `arn:aws:events:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:rule/${CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME}`,
+          StatementId: `cloud-watch-event-${CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME}`
         }).promise()
       })
       .then(()=>{
         return cloudwatchevents.putTargets({
-          Rule: CLOUDWATCH_NIGHT_JOB_SCHEDULER_NAME,
+          Rule: CLOUD_WATCH_NIGHT_JOB_SCHEDULER_NAME,
           Targets:[{
-            Id: `lambda-${downloadExchangeRateCronJobName}-starter`,
-            Arn: `arn:aws:lambda:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:function:${downloadExchangeRateCronJobName}`
+            Id: `lambda-${LAMBDA_DOWNLOAD_EXCHANGE_RATE_CRON_JOB_NAME}-starter`,
+            Arn: `arn:aws:lambda:${config.env.AWS_REGION}:${config.env.AWS_ACCOUNT}:function:${LAMBDA_DOWNLOAD_EXCHANGE_RATE_CRON_JOB_NAME}`
           }]
         }).promise()
       });
-  }).catch(Promise.resolve);
+  });
 }
 
 
