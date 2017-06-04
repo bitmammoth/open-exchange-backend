25518d6e6a33dbc48a5bcc23c235f9e482ce7a3b
David Ng <david.ng.dev@gmail.com>
2017-06-04 21:48:35 +0800

    Add comment to GraphQL and add example link for GraphQL to readme

--- model/schema.js
+++ model/schema.js
@@ -8,26 +8,39 @@ const { buildSchema } = require('graphql');
 
 let schema = buildSchema(`
   type Query {
+    # Will return exchange rate of currency in given date range
     historicalExchangeRate(dateRange: DateRange, baseCurrency: String, pageToken: String): ExchangeRateCollection
+    # Will return exchange rate of currency in least sync job executed date
     leastExchangeRate(baseCurrency: String): ExchangeRateCollection
+    # Will return conversion rate between currency in given date range
     historicalConversionRate(dateRange: DateRange, currencyConvert: CurrencyConvert, pageToken: String): ConversionRateCollection
+    # Will return conversion rate between currency in given date range
     leastConversionRate(currencyConvert: CurrencyConvert): ConversionRateCollection
   }
   
+  # Exchange rate result
   type ExchangeRateCollection {
+    # Base currency
     base: String 
+    # Start date
     from: String
+    # End date
     to: String
+    # List of exchange rate
     rates: [ExchangeRate]
+    # Will null if no more data can be provided 
     next_page_token: String
   }
   
+  # Exchange rate by day
   type ExchangeRate {
+    # Exchange rate effected date
     date: String,
     rate: String,
     currency: String
   }
   
+  # Conversion rate result
   type ConversionRateCollection {
     base: String 
     targetCurrency: String
